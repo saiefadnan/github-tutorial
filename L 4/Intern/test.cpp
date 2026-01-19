b1d1bb10d63574fdf0a2094b380bca798def5af4
Sakif Shahrear <sakifshahrear@gmail.com>
2026-01-19 17:10:33 +0600

    a

--- L 4/Intern/test.cpp
+++ L 4/Intern/test.cpp
@@ -12,7 +12,20 @@ int32_t main()
     int count = 0;
     for (int j = 1; j * j <= n; j++)
     {
+        if (n % j == 0)
+        {
+            int other_divisor = n / j;
+            if (j * j != n)
+            {
+                a.push_back(j);
+                a.push_back(other_divisor);
+            }
+            else if (j * j == n)
+            {
+                a.push_back(j);
+            }
         }
+    }
     sort(a.begin(), a.end());
     if (k > a.size())
     {
